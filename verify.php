7d36511fec4f8fc9e423e17cc844a21a4e649352
Md. Daud Hossain Supto <109514701+mddaudhossainsupto@users.noreply.github.com>
2024-04-16 22:12:53 +0600

    verify.php

--- verify.php
+++ verify.php
@@ -2,7 +2,7 @@
 	$email = $_POST['inputEmail'];
 	$pswd = $_POST['inputPasswd'];
 
-	$conn = mysqli_connect("localhost", "root", "", "www_project");
+	$conn = mysqli_connect("localhost", "root", "", "dhsupto");
 	if(!$conn){
 		echo "Cannot connecto to database " . mysqli_connect_error($conn);
 		exit;
